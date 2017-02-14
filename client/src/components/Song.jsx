467b52662e5b1ca1ed08f148e4a85ef4e739493b
Elgrogz <gregorgilchrist@gmail.com>
2017-02-14 22:58:32 +0000

    can get song data and basic Song set up

--- client/src/components/Song.jsx
+++ client/src/components/Song.jsx
@@ -1,4 +1,19 @@
 var React = require('react');
 
+var Song = function(props) {
+
+  if (!props.song) {
+    return
+  }
+
+  return (
+    <div className="song-details">
+    <h4>Song title: </h4>
+    <h4>Chart Position: </h4>
+    <h4>Artist: </h4>
+    </div>
+    )
+
+}
 
 module.exports = Song;
--- client/src/containers/SongList.jsx
+++ client/src/containers/SongList.jsx
@@ -7,10 +7,23 @@ var SongList = React.createClass({
     return {songs: []};
   },
 
+  componentDidMount: function() {
+    var url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = function() {
+      if (request.status === 200) {
+        var data =JSON.parse(request.responseText);
+        this.setState({songs: data.feed.entry});
+      }
+    }.bind(this);
+    request.send();
+  },
+
   render: function() {
     return (
         <div className="song-list">
-        <p>Hello</p>
+
         </div>
       )
   }
